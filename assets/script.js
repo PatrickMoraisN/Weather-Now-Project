c859c90f517ec7bf38b796ef33b36568fb7f6ddc
Patrick Morais <ppternunes@gmail.com>
2021-04-17 11:37:07 -0300

    finalizando modal, e icons

--- assets/script.js
+++ assets/script.js
@@ -6,25 +6,49 @@ const tempElem = document.querySelector('.temp-fate');
 const modalText = document.querySelector('.modal-text');
 const modal = document.querySelector('.modal-container');
 const climaElem = document.querySelector('.clima');
+const imgIcon = document.querySelector('.icon')
+const citySpan = document.querySelector('.local');
+const greetsSpan = document.querySelector('.greet');
+const smallPhrase = document.querySelector('.small-phrase');
 const apiWeather = {
   key: '61ef00c8825fd4245bfd38468e696a37',
   unit: 'metric',
   lang: 'pt_br',
 }
 
-const showWeather = (temperature, weather) => {
+const randomPhrase = () => {
+  const phrases = [
+    'Vai uma pizza frita ai?',
+    'Qual a boa de hoje?',
+    'Vamos com tudo!',
+    'Foguete nao tem ré',
+    'Avante trybengerss!!'
+  ]
+  const randomIndex = Math.floor(Math.random() * 5)
+  if (randomIndex !== 5) {
+    smallPhrase.innerHTML = `${phrases[randomIndex]}`;
+  }
+}
+
+const showWeather = (temperature, weather, icon, city) => {
+  const greets = ['Olá', 'Eai', 'Coee', 'Faala', 'Oioi'];
+  const randomIndex = Math.floor(Math.random() * 4);
   const tempCelsius = Math.round(temperature - 273, 15);
   tempElem.innerHTML = `${tempCelsius}`;
   climaElem.innerHTML = `${weather}`;
+  imgIcon.src = `./assets/imgs/icons/${icon}.png`;
+  citySpan.innerHTML = city;
+  greetsSpan.innerHTML = `${greets[randomIndex]}`;
+  randomPhrase();
 }
 const getWeather = async (city) => {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${apiWeather.lang}&unit=${apiWeather.unit}&appid=${apiWeather.key}`);
   const objs = await response.json()
-  showWeather(objs.main.temp, objs.weather[0].description);
+  showWeather(objs.main.temp, objs.weather[0].description, objs.weather[0].icon, city);
 }
 
 const checkInput = () => {
-    checkInputP.innerHTML = 'Cep/Localidade invalidos! <br> tente 22011010 ou Rio de Janeiro por ex :)';
+  checkInputP.innerHTML = 'Cep/Localidade invalidos! <br> tente 22011010 ou Rio de Janeiro por ex :)';
 }
 
 const checkNum = () => {
